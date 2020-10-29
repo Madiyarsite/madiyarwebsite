e5796a877f8967aa731a5e6a8a3a581013e80eb4
Madiyar <madiyaresmuhambetov7@gmail.com>
2020-10-29 10:54:02 +0600

    jav

---
+++ js/js.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let liken;
+function likee(){
+    liken = confirm("Вам нравится этот сайт?");
+    if(liken == true){
+        alert("Хорошо");
+    }else{
+        alert("Напишите мне на почту как мне это исправить");
+    }
+}
+likee();
